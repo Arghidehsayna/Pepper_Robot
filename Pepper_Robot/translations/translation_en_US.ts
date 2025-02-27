038f414c74685a7177235c029b77218eb0b31240
Sayna Arghideh <152231092+Arghidehsayna@users.noreply.github.com>
2025-02-27 14:52:51 +0000

    Add files via upload

---
+++ Pepper_Robot/translations/translation_en_US.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/BoyRegistration/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Here you are! It is a catalog of our gym.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Here you are! It is a catalog of our gym.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GirlRegistration/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>You are so well-shaped, I think this gym will help you stay in shape more.
+</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You are so well-shaped, I think this gym will help you stay in shape more.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Hello/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hi, welcome to Sport Piu Club!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi, welcome to Sport Piu Club!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Hello/Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hi, welcome to Sport Piu Club!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hi, welcome to Sport Piu Club!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You can use the promotion, so you only need to pay 900 euros for a year and enjoy one month of free tennis court access.
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You can use the promotion, so you only need to pay 900 euros for a year and enjoy one month of free tennis court access.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You should pay 130 euros for a month and will receive a free gym bag with slippers as a gift.
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You should pay 130 euros for a month and will receive a free gym bag with slippers as a gift.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Your registration is complete.I hope you have an enjoyable time at our gym.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Your registration is complete.I hope you have an enjoyable time at our gym.</translation>
+        </message>
+    </context>
+</TS>
